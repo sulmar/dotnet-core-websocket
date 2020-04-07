e4089ab24adeb4ee76b1fec532d4ac122328e136
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-04-07 10:43:41 +0200

    Utworzenie metody MapWebSocket

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -7,11 +7,69 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Api
 {
+    public class MyDashboardMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public MyDashboardMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                await Echo(webSocket);
+
+                return;
+            }
+            await next(context);
+        }
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+
+    public static class MyEndpointRouteBuilderExtensions
+    {
+        public static IEndpointConventionBuilder MapWebSocket(
+                 this IEndpointRouteBuilder endpoints,
+                 string pattern,
+                 Func<WebSocket, Task> execute = null)
+        {
+
+            return endpoints.Map(pattern, async (context) =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                    await execute?.Invoke(webSocket);
+                }
+            });
+        }
+    }
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -20,7 +78,7 @@ namespace Api
         {
         }
 
-    
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,24 +93,58 @@ namespace Api
 
             app.UseRouting();
 
-            app.Map("/ws", builder =>
+            //app.Map("/ws", builder =>
+            //{
+            //    builder.Use(async (context, next) =>
+            //    {
+            //        if (context.WebSockets.IsWebSocketRequest)
+            //        {
+            //            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            //            await Echo(webSocket);
+            //            return;
+            //        }
+            //        await next();
+            //    });
+            //});
+
+            app.UseEndpoints(endpoints =>
             {
-                builder.Use(async (context, next) =>
-                {
-                    if (context.WebSockets.IsWebSocketRequest)
-                    {
-                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                        await Echo(context, webSocket);
-                        return;
-                    }
-                    await next();
-                });
+                //endpoints.Map("/ws", async (context) =>
+                //{
+                //    if (context.WebSockets.IsWebSocketRequest)
+                //    {
+                //        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                //        await Echo(webSocket);
+                //    }
+                //});
+
+                endpoints.MapWebSocket("/ws", async websocket => await Echo(websocket));
+
+                endpoints.Map("/", async context => await context.Response.WriteAsync("Hello World!"));
+
             });
         }
 
         private async Task Echo(HttpContext context, WebSocket webSocket)
         {
             var buffer = new byte[1024 * 4];
+
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+
+            // var webSocket = await context.WebSockets.AcceptWebSocketAsync();
 
             WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             while (!result.CloseStatus.HasValue)
